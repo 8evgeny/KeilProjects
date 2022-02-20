0ad2fd2c674da8fc1eb1d86869e46fbae802c854
8evgeny <8evgeny@gmail.com>
2022-02-20 15:09:55 +0300

    Project_1_F103C8T6_blink

---
+++ F103C8T6_1/Objects/stm32f10x_usart.d
@@ -0,0 +1,17 @@
+./objects/stm32f10x_usart.o: pf\src\stm32f10x_usart.c \
+  pf\inc\stm32f10x_usart.h pf\stm32f10x.h \
+  C:\Users\evg\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\core_cm3.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Users\evg\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\evg\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\evg\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Users\evg\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.4.0\Device\Include\system_stm32f10x.h \
+  pf\stm32f10x_conf.h pf\inc\stm32f10x_adc.h pf\inc\stm32f10x_bkp.h \
+  pf\inc\stm32f10x_can.h pf\inc\stm32f10x_cec.h pf\inc\stm32f10x_crc.h \
+  pf\inc\stm32f10x_dac.h pf\inc\stm32f10x_dbgmcu.h \
+  pf\inc\stm32f10x_dma.h pf\inc\stm32f10x_exti.h \
+  pf\inc\stm32f10x_flash.h pf\inc\stm32f10x_fsmc.h \
+  pf\inc\stm32f10x_gpio.h pf\inc\stm32f10x_i2c.h pf\inc\stm32f10x_iwdg.h \
+  pf\inc\stm32f10x_pwr.h pf\inc\stm32f10x_rcc.h pf\inc\stm32f10x_rtc.h \
+  pf\inc\stm32f10x_sdio.h pf\inc\stm32f10x_spi.h pf\inc\stm32f10x_tim.h \
+  pf\inc\stm32f10x_wwdg.h pf\inc\misc.h
---
+++ F103C8T6_1/user/main.h
@@ -0,0 +1,5 @@
+#ifndef main_h_
+#define main_h_
+void delay(long cycles);
+void gpio_ports_enable(void);
+#endif
